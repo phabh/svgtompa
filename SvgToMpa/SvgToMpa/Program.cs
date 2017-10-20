1cb403d8280744ae0bd7f84e8fe66abef10c8ccc
Paulo Henrique Abreu (Take.net) <pauloh@takenet.com.br>
2017-10-20 16:15:31 -0200

    init commit

---
+++ SvgToMpa/SvgToMpa/Program.cs
@@ -0,0 +1,27 @@
+﻿using Svg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SvgToMpa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var svgFile = SvgDocument.Open("D:\\Frame.svg");
+
+            var regex = new Regex(@"^id(?<flow>\d*\.?\d*?\.?\d*?)_(?<tree>\d*\.?\d*?\.?\d*?)_(?<type>Text)");
+
+            var elements = svgFile.Descendants().Where(e => e.ID != null && regex.IsMatch(e.ID) ).ToList();
+         
+            elements[0].Children
+
+
+
+        }
+    }
+}
