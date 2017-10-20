f864432e6d858671182c111169f95a56cc557e2a
Paulo Henrique Abreu (Take.net) <pauloh@takenet.com.br>
2017-10-20 19:12:29 -0200

    implementing blocktext

---
+++ SvgToMpa/SvgToMpa/Block.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SvgToMpa
+{
+    public class Block
+    {
+        public string NavigationName { get; set; }
+        public string FlowPosition { get; set; }
+        public string Type { get; set; }
+    }
+}
--- SvgToMpa/SvgToMpa/Program.cs
+++ SvgToMpa/SvgToMpa/Program.cs
@@ -1,27 +1,49 @@
 ﻿using Svg;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace SvgToMpa
 {
     class Program
     {
+        static Regex regex = new Regex(@"^id(?<flow>\d*\.?\d*?\.?\d*?)_(?<tree>\d*\.?\d*?\.?\d*?)_(?<type>Text)");
+
         static void Main(string[] args)
         {
-            var svgFile = SvgDocument.Open("D:\\Frame.svg");
+            StreamReader reader = new StreamReader("D:\\Frame.svg", Encoding.UTF8);
 
-            var regex = new Regex(@"^id(?<flow>\d*\.?\d*?\.?\d*?)_(?<tree>\d*\.?\d*?\.?\d*?)_(?<type>Text)");
+            var svgFile = SvgDocument.Open<SvgDocument>(reader.BaseStream);
 
-            var elements = svgFile.Descendants().Where(e => e.ID != null && regex.IsMatch(e.ID) ).ToList();
-         
-            elements[0].Children
+            var elements = svgFile.Descendants().Where(e => e.ID != null && regex.IsMatch(e.ID)).ToList();
 
+            var blocks = GetBlocks(elements);
 
 
         }
+
+        private static IEnumerable<BlockText> GetBlocks(IEnumerable<SvgElement> elements)
+        {
+            return elements.Descendants().Where(e => e.ID != null && e.ID.Contains("#t="))
+                .Select(e => 
+                        {
+                            var match = regex.Match(e.Parent.ID);
+
+                            return new BlockText()
+                            {
+                                NavigationName = match.Groups["flow"].Value,
+                                FlowPosition = match.Groups["tree"].Value,
+                                Type = match.Groups["type"].Value,
+                                Text =  Encoding.UTF8.GetString(Encoding.Default.GetBytes(e.ID.Substring(3)))
+                            };
+                        });
+        }
+
+
     }
 }
